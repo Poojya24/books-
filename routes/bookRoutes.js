6bb227e24723ff66499a4c31d204b9b55dda1829
Poojya Lal <134841187+Poojya24@users.noreply.github.com>
2026-01-23 16:47:35 +0530

    Update bookRoutes.js

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -1,13 +1,18 @@
-const express = require("express");
-const { getBooks, createBooks, getBooksbyId, deleteBookbyId, updateBookbyId } = require("../controller/bookController");
+ const express = require("express");
+const {
+  getBooks,
+  createBooks,
+  getBooksbyId,
+  deleteBookbyId,
+  updateBookbyId
+} = require("../controller/bookController");
 
 const router = express.Router();
-const authmiddleware = require("../middleware/authmiddleware")
 
 router.get("/", getBooks);
-router.post("/", authmiddleware, createBooks);
-router.get("/:id", getBooksbyId)
-router.delete("/:id", authmiddleware, deleteBookbyId);
-router.put("/:id", authmiddleware, updateBookbyId);
+router.post("/", createBooks);
+router.get("/:id", getBooksbyId);
+router.delete("/:id", deleteBookbyId);
+router.put("/:id", updateBookbyId);
 
 module.exports = router;
