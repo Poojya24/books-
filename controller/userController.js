ee61cb5f57ea8b269b3a2af65bdbe7d3ade98cfc
Poojya Lal <134841187+Poojya24@users.noreply.github.com>
2026-01-23 15:32:11 +0530

    Update userController.js

--- controller/userController.js
+++ controller/userController.js
@@ -9,29 +9,31 @@ const createToken = (id) => {
     return jwt.sign({ _id: id }, process.env.JWT_SECRET, { expiresIn: "1d", });
 };
 
- const signupUser = async (req, res) => {
+  const signupUser = async (req, res) => {
   try {
-    const { email, password } = req.body;
+    const { name, email, password } = req.body;
 
-    if (!email || !password) {
-      return res.status(400).json({ message: "All fields required" });
+    if (!name || !email || !password) {
+      return res.status(400).json({ message: "All fields are required" });
     }
 
-    const exists = await User.findOne({ email });
-    if (exists) {
+    const userExists = await User.findOne({ email });
+    if (userExists) {
       return res.status(400).json({ message: "User already exists" });
     }
 
-    const user = await User.create({ email, password });
+    const user = await User.create({ name, email, password });
 
     res.status(201).json({
-      id: user._id,
-      email: user.email,
-      token: createToken(user._id),
+      message: "User registered successfully",
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email
+      }
     });
-
   } catch (error) {
-    res.status(500).json({ message: "Signup failed" });
+    res.status(500).json({ message: error.message });
   }
 };
 
